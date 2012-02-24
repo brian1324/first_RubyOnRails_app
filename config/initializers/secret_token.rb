ce0b762e67448b4fa9392f551bf840cdade68bf0
Brian O'Connor <brian.oconn88@gmail.com>
2012-02-24 17:34:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogo1::Application.config.secret_token = 'c1ae7e03118c73e959f360f2e3db0228166a71c3fb9327e61a7c1406bd14ee339bdd373414d5f3be09370cca4d16beaba272f23abdc4a43658843b115f34ff1c'
